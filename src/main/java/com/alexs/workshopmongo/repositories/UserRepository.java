568fea687c4c467aa05a47edabd299f2fda6f45d
Alexsander de Morais <alexsanderm@kyros.com.br>
2024-07-22 22:04:29 -0300

    Connecting to MongoDB with repository and service

---
+++ src/main/java/com/alexs/workshopmongo/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.alexs.workshopmongo.repositories;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.alexs.workshopmongo.domain.User;
+
+public interface UserRepository extends MongoRepository<User, String>{
+	public List<User> findAll();
+}
--- src/main/java/com/alexs/workshopmongo/resources/UserResource.java
+++ src/main/java/com/alexs/workshopmongo/resources/UserResource.java
@@ -1,29 +1,31 @@
 package com.alexs.workshopmongo.resources;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alexs.workshopmongo.domain.User;
+import com.alexs.workshopmongo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
-public class UserResource implements Serializable{
+public class UserResource implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	@Autowired
+	private UserService service;
+
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
-		User teste = new User("1", "Teste", "teste@teste.com");
-		List<User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(teste));
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
+		
 	}
-	
+
 }
