86a7402891826839b077658dd55a62284718dd6a
Alexsander de Morais <alexsanderm@kyros.com.br>
2024-07-26 21:27:36 -0300

    Search using more than 1 argument

--- src/main/java/com/alexs/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/alexs/workshopmongo/repositories/PostRepository.java
@@ -1,5 +1,6 @@
 package com.alexs.workshopmongo.repositories;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -10,11 +11,15 @@ import com.alexs.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
-	public List<Post> findAll();
+	List<Post> findAll();
 	
-	public List<Post> findByTitleContaining(String str);
+	List<Post> findByTitleContaining(String str);
 	
 	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
-	public List<Post> searchTitle(String str);
-
+	List<Post> searchTitle(String str);
+	
+	
+	@Query("{ $and:[{'date':{$gte: ?1}} ,{'date':{$lte: ?2}} ,{$or: [{'title': { $regex: ?0, $options: 'i' }}, {'body': { $regex: ?0, $options: 'i' }},{'comments.text': { $regex: ?0, $options: 'i' }}]}]}")
+	List<Post> fullSearch(String str, Date minDate, Date maxDate);
+	
 }
